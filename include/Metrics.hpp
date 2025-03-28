6f28a39863528ca9903f1e34a9a588e3212541f1
Ellipsis <rizkyfathur326@gmail.com>
2025-03-29 05:54:14 +0700

    feat: defined Metrics

---
+++ include/Metrics.hpp
@@ -0,0 +1,41 @@
+#ifndef __METRICS_HPP__
+#define __METRICS_HPP__
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+#include "Color.hpp"
+
+class Metric {
+ public:
+  virtual double compute(const std::vector<std::vector<Color>>& pixels, int x,
+                         int y, int width, int height) = 0;
+  virtual ~Metric() {}
+};
+
+class VarianceMetric : public Metric {
+ public:
+  double compute(const std::vector<std::vector<Color>>& pixels, int x, int y,
+                 int width, int height) override;
+};
+
+class MADMetric : public Metric {
+ public:
+  double compute(const std::vector<std::vector<Color>>& pixels, int x, int y,
+                 int width, int height) override;
+};
+
+class MaxPixelDifferenceMetric : public Metric {
+ public:
+  double compute(const std::vector<std::vector<Color>>& pixels, int x, int y,
+                 int width, int height) override;
+};
+
+class EntropyMetric : public Metric {
+ public:
+  double compute(const std::vector<std::vector<Color>>& pixels, int x, int y,
+                 int width, int height) override;
+};
+
+#endif
